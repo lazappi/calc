0c2e4e70b7b6a30783943fa78f961a04ee7a0545
sahas111 <susmi06@yahoo.com>
2014-12-11 15:35:43 +1100

    add max

--- calc.py
+++ calc.py
@@ -1,8 +1,8 @@
-"""calc.py: A simple Python calculator."""
+""""calc.py: A simple Python calculator."""
 import sys
 
 def add_all(nums):
-    return sum (nums)
+    return sum(nums)
 
 def multiply_all(nums):
     return reduce(lambda a, b: a * b, nums)
@@ -11,11 +11,13 @@ if __name__ == '__main__':
     command = sys.argv[1]
     nums = map(float, sys.argv[2:])
     if command == 'add':
-        print (add_all(nums))
+        print(add_all(nums))
     elif command == 'multiply':
         print(multiply_all(nums))
     elif command == 'min':
         print(min(nums))
+    elif command == 'max':
+        print(max(nums))
     else:
         usage = "calc.py [add|multiply|min] NUM1 [NUM2 [NUM3 [...]]]"
         print(usage)
