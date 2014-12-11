433c2a9e03efb1e068cfff3384359bd2cd8a4703
sahas111 <susmi06@yahoo.com>
2014-12-11 16:14:26 +1100

    diff

--- calc.py
+++ calc.py
@@ -7,20 +7,27 @@ def add_all(nums):
 def multiply_all(nums):
     return reduce(lambda a, b: a * b, nums)
 
+
 def divide_all(nums):
     return reduce(lambda a, b: a / b, nums)
+
+def diff_all(nums):
+    return reduce(lambda a, b: a - b, nums)
+
 
 if __name__ == '__main__':
     command = sys.argv[1]
     nums = map(float, sys.argv[2:])
     if command == 'add':
         print(add_all(nums))
+    elif command == 'subtract':
+        print(diff_all(nums))
     elif command == 'multiply':
         print(multiply_all(nums))
     elif command == 'divide':
         print(divide_all(nums))
     elif command == 'min':
-        print(min(nums))
+         print(min(nums))
     elif command == 'max':
         print(max(nums))
     else:
